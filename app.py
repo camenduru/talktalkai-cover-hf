6782125dbfe24b0233635e999f970d3df7d204fb
Kevin Wang <kevinwang676@users.noreply.huggingface.co>
2024-06-19 07:39:26 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import spaces
 
 import os
-os.system("pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 --index-url https://download.pytorch.org/whl/cu117")
+#os.system("pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 --index-url https://download.pytorch.org/whl/cu117")
 import glob
 import json
 import traceback
